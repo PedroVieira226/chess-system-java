16ac2053bace4e9c27cfd47a5d0fd9c18a73e8eb
Pedro <pedrovieira1355@gmail.com>
2024-10-08 16:17:48 -0300

    BoardException and defensive programming

---
+++ src/boardgames/BoardException.java
@@ -0,0 +1,9 @@
+package boardgames;
+
+public class BoardException extends RuntimeException {
+
+    public BoardException(String message) {
+        super(message);
+    }
+    private static final long serialVersionUID = 1L;
+}
