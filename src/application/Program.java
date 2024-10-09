0b050f64c8c898d11393aa94052cc985052213b7
Pedro <pedrovieira1355@gmail.com>
2024-10-09 03:43:59 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,31 @@
 package application;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         ChessMatch match = new ChessMatch();
-        UI.printBoard(match.getpieces());
+
+        while(true){
+            UI.printBoard(match.getpieces());
+            System.out.println();
+            System.out.println("Source");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.println("Target");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = match.performChessMove(source, target );
+
+        }
+
     }
 
 
--- src/boardgames/Board.java
+++ src/boardgames/Board.java
@@ -66,5 +66,19 @@ public class Board {
         return piece(position) != null;
     }
 
+    public Piece removePiece(Position position){
+        if(!PositionExists(position)){
+            throw new BoardException("Postion not on the board");
+        }
+        if(piece(position) == null){
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return aux;
+    }
+
+
 
 }
